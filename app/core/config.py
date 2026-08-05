34537cdc6b14f50b672916fa4105eb611a2af5f6
wyn-code <brunoo6.massocco@gmail.com>
2026-08-05 13:30:22 -0300

    a

--- app/core/config.py
+++ app/core/config.py
@@ -32,7 +32,7 @@ class Settings(BaseSettings):
     CORS_ORIGINS: list[str] = [
         "http://localhost:5173",
         "http://localhost:3000",
-        "http://localhost:8080/"
+        "http://localhost:8080/",
         "https://calendar-proyect-one.vercel.app",
     ]
 
